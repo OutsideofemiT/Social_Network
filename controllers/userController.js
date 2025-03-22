3f3d16c6d345f04f409c1806176dfd4a0cee9738
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-03-21 21:08:26 -0500

    added createUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,11 @@ export const getUsers = async (req, res) => {
 
 	export const createUser = async (req, res) => {
 		try {
-			const newUser = await User.newUser(r)
+			const newUser = await User.create(req.body);
+			res.status(201).json(newUser);
+		}   catch (error) {
+			res.status(500).json({error.})
+		}
 		}
 	}
   
