3d6759cd673ed5a980e37914f044e4fe96269f63
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-03-21 20:48:15 -0500

    adding controllers

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+import User from '../models/User.js';
+
+export const getUsers = async (req, res) => {
+	try {
+	  const users = await User.find();
+	  res.status(200).json(users);
+	} catch (error) {
+	  res.status(500).json({ error: error.message });
+	}
+  };
+
+  export const getSingleUser = async (req, res) => {
+	try {
+		const user = await User.findbyId(req.params.userId);
+		if (!user) {	
+		return res.status(404).json({ message: 'User not found'});	
+	 }
+		res.status(200).json(users);
+	}	catch (error) {
+		res.status(500).jsob({error: error.message });
+	 }
+	};
+
+	export const createUser = async (req, res) => {
+		try {
+			const newUser = await User.newUser(r)
+		}
+	}
+  
