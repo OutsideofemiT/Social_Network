a85841e804699eb1e64dcb8ab88991460b6c980d
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-03-22 21:07:22 -0500

    added removefriend function to usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,7 +74,20 @@ export const getUsers = async (req, res) => {
 		}
 	};
 	
+	export const removeFriend = async (req, res) => {
+		try {
+			const user = await User.findByIdAndUpdate(
+				req.params.userId,
+				{ $pull: { friends: req.params.friendId } },
+				{ new: true }
+			);
+			if (!user) {
+				return res.status(404).json({ message: 'User not found' });
+			}
+			res.status(200).json(user);
+		} catch (error) {
+		  res.status(500).json({ error: error.message });
+		}
+	  };
 
-
-	//addFriend
-	//removeFriend
+		
