65e0219534b7c1b4b34f28d2567b44243fe4dcb6
Carmen Wheeler <carmenwh33l3r@gmail.com>
2025-03-22 22:07:47 -0500

    added user.js code

--- models/User.js
+++ models/User.js
@@ -1,13 +1,43 @@
+import mongoose from 'mongoose';
 
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+	type: string,
+	required: true,
+	unique: true,
+	match: [/.+@.+\..+/, 'Please enter a valid email address'],
+  },
+  thoughts: [
+	{
+	 type: mongoose.Schema.Types.ObjectId,
+	 ref: 'Thought',
+	},
+  ],
+  friends: [
+	{
+	 type: mongoose.Schema.Types.ObjectId,
+	 ref: 'User',
+	},
+  ],
+},
+{
+  toJSON: {
+	virtuals: true,
+	},
 
+	id: false,
+});
 
+userSchema.virtual('friendCount').get(function () {
+	return tis.friends.length;
+});
 
-
-
-
-
-
-
-
+const User = mongoose.model('User', userSchema);
 
 export default User.js
